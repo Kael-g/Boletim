132952d691b8ee8026f1991d4fe76cae6a30c5e9
Kael-g <mikael.gualdi@zup.com.br>
2023-06-01 16:17:32 -0300

    Mapa com string e lista - sobrescrevendo valores de outras chaves

---
+++ src/boletim.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class boletim {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Map<String, double[]> boletim = new HashMap<>();
+        double[] notas = new double[2];
+        String nome;
+
+        System.out.println("Boletim vazio");
+        System.out.print("Deseja adicionar novo aluno e suas notas? (S/N) ");
+        for(String adiciona = input.next(); adiciona.equalsIgnoreCase("s"); adiciona = input.next()){
+            System.out.print("Qual o nome do aluno? ");
+            nome = input.next();
+            for (int i = 0; i<2; i++){
+                System.out.print("Digite a nota "+ (i+1)+": ");
+                notas[i] = input.nextDouble();
+            }
+            System.out.println("Nome lido: "+ nome + "  notas digitadas: " + Arrays.toString(notas));
+            boletim.put(nome,notas);
+            System.out.printf("Put no map: %s  %s", nome, Arrays.toString(boletim.get(nome)));
+            System.out.print("\n\nDeseja adicionar novo aluno e suas notas? (S/N) ");
+        }
+
+        System.out.println("\nBoletim: ");
+
+        for(String key : boletim.keySet()){
+            System.out.printf("%-30s",key);
+            for(double nota : boletim.get(key)){
+                System.out.printf("%-5s", nota);
+            }
+            System.out.println();
+        }
+    }
+}
